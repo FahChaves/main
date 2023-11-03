d6d90d617f82ed9fdbd0b681b1a844de56e14866
FahChaves <147733409+FahChaves@users.noreply.github.com>
2023-11-03 09:19:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,63 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include "cadastro.h"
+#include <locale.h>
 #include "visual.h"
 
-#define TRUE 1
-#define FALSE 2
+main() {
 
-int main(){
-    setlocale(LC_ALL,"");
-    int p1, p2, p3,p4, cpf;
+    setlocale(LC_ALL, "Portuguese");
+
+    int cadastrarLoop;
+    struct Visitante visitante;
+
+
+    FILE *arquivo = fopen("visitantes.txt", "w");
+    if (arquivo != NULL) {
+        fclose(arquivo);
+    } else {
+        printf("Não foi possível abrir.\n");
+        return 1;
+    }
+    do {
+    printf("\n---------------------------");
+    printf("\nCadastro do visitante\n");
+    registrarVisitante(&visitante);
+
+    printf("\n=========================");
+    printf("\nInformações do visitante:\n");
+    printf("Nome - %s\n", visitante.nome);
+    printf("CPF - %s\n", visitante.cpf);
+    printf("Idade - %d\n", visitante.idade);
+    printf("Tipo de Ingresso - %s\n", visitante.tipoIngresso);
+    printf("=========================\n");
+
+   criarRecibo(visitante);
+
+    printf("\nHá mais algum visitante a ser cadastrado?\n");
+    printf("Digite a tecla 1 se sim. Digite 2 se não:");
+    scanf(" %d", &cadastrarLoop);
+
+    } while (cadastrarLoop == 1);
+
+   int p1,p2,p3,p4;
+   int escolha;
     do{
+    FILE *pesquisa = fopen("gravar.txt", "a");
+        if (pesquisa == NULL){
+            printf("Não foi possivel abrir o arquivo gravado...");
+        return 1;
+        }
+
+        printf("Aquivo não pode ser aberto");
+
+
+
         iniciarPesquisa();
         pPergunta();
         scanf("%d", &p1);
-
             switch(p1){
             case 1:
-            puts("Extremamente Satisfeito(a);");
+            puts ("Extremamente Satisfeito(a);");
             break;
             case 2:
             puts("Satisfeito(a)");
@@ -85,7 +126,20 @@ int main(){
             puts("Completamente insatisfeito");
             break;
             }
-        return(cpf);
-        limparTela();
-    }while(TRUE);
+            limparTela();
+        fprintf(pesquisa,"Resposta da primeira pergunda: %d", p1);
+        fprintf(pesquisa,"Resposta da segunda pergunda: %d", p2);
+        fprintf(pesquisa,"Resposta da terceira pergunda: %d",p3);
+        fprintf(pesquisa,"Resposta da quarta pergunda: %d", p4);
+
+        fclose(pesquisa);
+
+        printf("Deseja continuar a pesquisa?/n Digite 1 para sim, ou Digite 0 para não");
+        scanf("%d",&escolha);
+        }while(escolha == 1);
 }
+
+
+
+
+
