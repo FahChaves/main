e4d145725dbaa61409c5d280eb17730241ef2083
FahChaves <147733409+FahChaves@users.noreply.github.com>
2023-11-09 23:18:41 -0300

    Update cadastro.h

--- cadastro.h
+++ cadastro.h
@@ -2,20 +2,24 @@
 #include <stdbool.h>
 #define CPF_TAMANHO 12
 #define IDADE_TAMANHO 3
-
+#define MAX_NOME_COMPLETO 100
+#define MAX_VISITANTES 100
 typedef struct cadastro Cadastro;
 
+
 struct Visitante {
-    char nome[50];
+    char nome[MAX_NOME_COMPLETO];
+    char sobrenome[120];
     char cpf[CPF_TAMANHO];
     int idade;
-    char tipoIngresso[20];
     int cadastrarLoop;
     int  localVisitante;
     int auxiliar;
 };
 
-salvarArquivo(struct Visitante visitante, char Gravar);
+salvarArquivo(struct Visitante visitante);
 struct Visitante carregarArquivo();
 void criarRecibo(struct Visitante visitante);
+void cadastroCliente();
+
 
